09f26ad26dc13cb4f8cba38342957d75d21f2193
zhangyuzhou <zhang940420??>
2019-10-25 02:50:35 +0800

    init

---
+++ simple-eureka-server/src/main/java/com/yuzhou/Application.java
@@ -0,0 +1,10 @@
+package com.yuzhou;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+
+}
